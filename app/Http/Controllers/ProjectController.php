6f144c3907a04384fa8b2d9196d73cf2702e0685
mdiktushar <md.tushar1804@gmail.com>
2023-09-26 13:48:26 +0600

    project and user controller function define

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,4 +7,20 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     //
+
+    public function createProject (Request $request) {
+
+    }
+
+    public function listProject () {
+
+    }
+
+    public function singleProject ($id) {
+
+    }
+
+    public function deleteProject ($id) {
+
+    }
 }
