4c529e83b02fce26a5f22e5fdd900ec9b635e411
mdiktushar <md.tushar1804@gmail.com>
2023-09-26 13:59:42 +0600

    route setup ;

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use PHPUnit\Framework\Attributes\PostCondition;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+
+
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('profile', [UserController::class, 'profile']);
+    Route::get('logout', [UserController::class, 'logout']);
+
+
+    Route::post('create-project', [PostCondition::class, 'createProject']);
+    Route::get('list-project', [PostCondition::class, 'listProject']);
+    Route::get('project/{id}', [PostCondition::class, 'singleProject']);
+    Route::delete('project/{id}', [PostCondition::class, 'deleteProject']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
