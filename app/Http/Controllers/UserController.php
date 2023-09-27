d073d083d1561db1c86c33964564912ae423baae
mdiktushar <md.tushar1804@gmail.com>
2023-09-27 13:09:12 +0600

    register api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     //
 
     public function register (Request $request) {
+        $request->validate([
+            'name' =>'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required| confirmed',
+            
+        ]);
 
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->phone_no = isset($request->phone_no) ? $request->phone_no : '';
+
+        $user->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'user created',
+        ], 200);
     }
 
     public function login (Request $request) {
